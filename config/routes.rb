0c9dc3b77adfaed5bd9098f00db50b33d770bc76
Shota Iguchi <e.iguchi1124@gmail.com>
2015-10-26 00:55:40 +0900

    Generate controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'sessions#new'
+
+  resources :sessions, only: %i(new create)
+  resources :messages, only: %i(index create)
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
