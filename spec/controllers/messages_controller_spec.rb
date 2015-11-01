5c06cfa6984ab3efc3587c5e884d93eda78def7e
Shota Iguchi <e.iguchi1124@gmail.com>
2015-11-01 18:04:05 +0900

    Remove initial generated controller tests

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,16 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe MessagesController, type: :controller do
+RSpec.describe MessagesController do
   describe 'GET #index' do
     it 'returns http success' do
       get :index
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe 'GET #create' do
-    it 'returns http success' do
-      get :create
       expect(response).to have_http_status(:success)
     end
   end
